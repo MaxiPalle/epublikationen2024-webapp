c1e625269bbd4ad58e6574a777e999e7b9049964
Jens Kruse <jens.kruse@avanade.com>
2024-10-23 14:13:16 +0200

    Update 'program.cs' to allow directory browsing in ~/docs/.

--- Program.cs
+++ Program.cs
@@ -1,25 +1,44 @@
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDirectoryBrowser();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
-app.UseRouting();
+var fileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.WebRootPath, "images"));
+var requestPath = "/docs";
+
+// Enable displaying browser links.
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = fileProvider,
+    RequestPath = requestPath
+});
+
+app.UseDirectoryBrowser(new DirectoryBrowserOptions
+{
+    FileProvider = fileProvider,
+    RequestPath = requestPath
+});
 
 app.UseAuthorization();
 
+app.MapDefaultControllerRoute();
 app.MapRazorPages();
 
 app.Run();
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,12 +3,3 @@
 
 // Write your JavaScript code.
 
-var adminManager = new ActiveXObject('Microsoft.ApplicationHost.WritableAdminManager');
-adminManager.CommitPath = "MACHINE/WEBROOT/APPHOST/Contoso";
-
-var directoryBrowseSection = adminManager.GetAdminSection("system.webServer/directoryBrowse",
-   "MACHINE/WEBROOT/APPHOST/Contoso");
-directoryBrowseSection.Properties.Item("enabled").Value = true;
-directoryBrowseSection.Properties.Item("showFlags").Value = "Date, Time, Size, Extension";
-
-adminManager.CommitChanges();
